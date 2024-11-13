1c1c40a71199f092fa9c3ec149028384db9bd9b4
LuSc15 <luca.schmieding@gmail.com>
2024-11-13 16:04:30 +0100

    Projektdateien hinzufügen.

---
+++ Feiertage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Feiertage
+{
+
+
+    public class Rootobject
+    {
+        public string status { get; set; }
+        public Feiertage[] feiertage { get; set; }
+    }
+
+    public class Feiertage
+    {
+        public string date { get; set; }
+        public string fname { get; set; }
+        public string all_states { get; set; }
+        public string bw { get; set; }
+        public string by { get; set; }
+        public string be { get; set; }
+        public string bb { get; set; }
+        public string hb { get; set; }
+        public string hh { get; set; }
+        public string he { get; set; }
+        public string mv { get; set; }
+        public string ni { get; set; }
+        public string nw { get; set; }
+        public string rp { get; set; }
+        public string sl { get; set; }
+        public string sn { get; set; }
+        public string st { get; set; }
+        public string sh { get; set; }
+        public string th { get; set; }
+        public string comment { get; set; }
+        public object augsburg { get; set; }
+        public object katholisch { get; set; }
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+
+namespace API_Feiertage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bundesländer = { "Baden-Württemberg BW", "Brandenburg BB", "Hessen HE", "NRW (NW)"}
+            HttpClient client = new HttpClient();
+            Console.WriteLine("Jahr eingeben:");
+            string jahr = Console.ReadLine();
+            Console.WriteLine("Bundesland eingeben:");
+            Console.WriteLine("Baden-Württemberg BW");
+            Console.WriteLine("Brandenburg BB");
+            Console.WriteLine("Baden-Württemberg BW");
+
+
+            Rootobject root = client.GetFromJsonAsync<Rootobject>("https://get.api-feiertage.de?years=2024&states="+bundesland+").Result;
+            foreach(Feiertage i in root.feiertage)
+            {
+                Console.WriteLine(i.fname + i.date);
+            }
+        }
+    }
+}
